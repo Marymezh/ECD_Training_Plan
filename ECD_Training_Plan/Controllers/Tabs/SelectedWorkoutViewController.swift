bf8c85b2417011f38a5841e16554647cfe73ef4b
Marymezh <maria.mezhova@yahoo.com>
2023-01-23 13:44:01 +0700

    working on commentTextView sizing on SelectedWorkoutVC

--- ECD_Training_Plan/Controllers/Tabs/SelectedWorkoutViewController.swift
+++ ECD_Training_Plan/Controllers/Tabs/SelectedWorkoutViewController.swift
@@ -11,7 +11,7 @@ class SelectedWorkoutViewController: UIViewController {
 
     private var commentsArray = ["Great workout, finished in 10:35 min", "It was too hard for me, only 3 rounds completed"]
 
-    let workoutTask = " 4 Rounds for time: \n\n 30 DU, \n 10 Squat Snatch, \n 15 HSPU, \n time cap - 12 min \n\n Post your results in the comments!"
+    private let workoutTask = " 4 Rounds for time: \n\n 30 DU, \n 10 Squat Snatch, \n 15 HSPU, \n time cap - 12 min \n\n Post your results in the comments!"
     
     private let workoutView: UIView = {
         let view = UIView()
@@ -36,49 +36,47 @@ class SelectedWorkoutViewController: UIViewController {
     
     private let tableView = UITableView(frame: .zero, style: .plain)
     
-    private let commentTextField: UITextView = {
-       let textField = UITextView()
-        textField.toAutoLayout()
-//        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
-//        textField.leftViewMode = .always
-        textField.font = UIFont.systemFont(ofSize: 12, weight: .regular)
-        textField.textColor = .black
-        textField.sizeToFit()
-        textField.backgroundColor = .white
-        textField.layer.borderWidth = 0.5
-        textField.layer.borderColor = UIColor.black.cgColor
-        textField.layer.cornerRadius = 5
-        //textField.placeholder = "Write your comment and result"
-        return textField
+    private let commentTextView: UITextView = {
+       let textView = UITextView()
+        textView.toAutoLayout()
+        textView.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        textView.textColor = .black
+        textView.tintColor = .black
+        textView.sizeToFit()
+        textView.isScrollEnabled = false
+        textView.backgroundColor = .white
+        textView.layer.borderWidth = 0.5
+        textView.layer.borderColor = UIColor.black.cgColor
+        textView.layer.cornerRadius = 5
+        return textView
     }()
     
     private let addCommentButton: UIButton = {
         let button = UIButton()
         button.toAutoLayout()
         button.tintColor = .black
- //       button.backgroundColor = .white
         button.setImage(UIImage(systemName: "paperplane.fill"), for: .normal)
         button.addTarget(self, action: #selector(commentSent), for: .touchUpInside)
         return button
     }()
     
     @objc func commentSent() {
-        
+        //to be done
     }
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         workoutDescriptionLabel.text = workoutTask
-        setUpNavBar()
         setupUI()
+        setupSubviews()
         setupTableView()
-        
     }
+
     
-    private func setUpNavBar() {
+    private func setupUI() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add comment", style: .plain, target: self, action: #selector(addComment))
+        view.backgroundColor = UIColor(named: "lightGreen")
     }
     
     private func setupTableView() {
@@ -89,37 +87,46 @@ class SelectedWorkoutViewController: UIViewController {
         tableView.toAutoLayout()
     }
     
-    private func setupUI() {
-        view.backgroundColor = UIColor(named: "lightGreen")
+    override func viewDidLayoutSubviews() {
+        let h = self.commentTextView.contentSize.height
+        let limit = CGFloat(200)
+        
+        if h > limit {
+            self.commentTextView.isScrollEnabled = true
+        } else {
+            commentTextView.isScrollEnabled = false
+        }
+    }
+    
+    private func setupSubviews() {
+        
         workoutView.addSubview(workoutDescriptionLabel)
-        view.addSubviews(workoutView, tableView, commentTextField, addCommentButton)
+        view.addSubviews(workoutView, tableView, commentTextView, addCommentButton)
         
         let constraints = [
             
             workoutView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
             workoutView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
             workoutView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            workoutView.bottomAnchor.constraint(equalTo: workoutDescriptionLabel.bottomAnchor, constant: 15),
             
             workoutDescriptionLabel.topAnchor.constraint(equalTo: workoutView.topAnchor, constant: 10),
             workoutDescriptionLabel.leadingAnchor.constraint(equalTo: workoutView.leadingAnchor, constant: 10),
             workoutDescriptionLabel.trailingAnchor.constraint(equalTo: workoutView.trailingAnchor, constant: -10),
+            workoutDescriptionLabel.bottomAnchor.constraint(equalTo: workoutView.bottomAnchor, constant: -10),
             
             tableView.topAnchor.constraint(equalTo: workoutView.bottomAnchor, constant: 15),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -75),
+
+            commentTextView.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 15),
+            commentTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            commentTextView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -65),
+            commentTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -5),
             
-            commentTextField.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 15),
-            commentTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            commentTextField.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -85),
-            commentTextField.heightAnchor.constraint(equalToConstant: 40),
-     //       commentTextField.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            
-            addCommentButton.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 15),
-            addCommentButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            addCommentButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
             addCommentButton.widthAnchor.constraint(equalToConstant: 40),
-            addCommentButton.heightAnchor.constraint(equalToConstant: 40)
+            addCommentButton.heightAnchor.constraint(equalToConstant: 40),
+            addCommentButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ]
         
         NSLayoutConstraint.activate(constraints)
@@ -146,15 +153,6 @@ extension SelectedWorkoutViewController: UITableViewDelegate, UITableViewDataSou
         return 1
     }
     
-//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        return "COMMENTS"
-//    }
-//
-//
-//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-//        return 25
-//    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return commentsArray.count
     }
@@ -176,5 +174,6 @@ extension SelectedWorkoutViewController: UITableViewDelegate, UITableViewDataSou
         return nil
     }
 }
+
     
 
