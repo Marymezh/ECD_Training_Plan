469f0e3c264645ba3bf5123325a64d99cf240b76
Marymezh <maria.mezhova@yahoo.com>
2023-01-25 10:46:19 +0700

    fixing current version, next - refactor the SelectedWorkoutVC into TableView

--- ECD_Training_Plan/Controllers/Tabs/SelectedWorkoutViewController.swift
+++ ECD_Training_Plan/Controllers/Tabs/SelectedWorkoutViewController.swift
@@ -11,7 +11,20 @@ class SelectedWorkoutViewController: UIViewController {
 
     private var commentsArray = ["Great workout, finished in 10:35 min", "It was too hard for me, only 3 rounds completed"]
 
-    private let workoutTask = " 4 Rounds for time: \n\n 30 DU, \n 10 Squat Snatch, \n 15 HSPU, \n time cap - 12 min \n\n Post your results in the comments!"
+    private let workoutTask = " 4 Rounds for time: \n\n 30 DU, \n 10 Squat Snatch, \n 15 HSPU, \n 10 Clean and Jerk, \n 100 DU, \n 20 box jump over, \n 30 Wall BAll, \n 10 Wall Climb, \n Air Squat, \n 10 m Handstand walk, \n time cap - 25 min \n\n Post your results in the comments! \n\n And don't forget to tag me %)"
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.toAutoLayout()
+        return scrollView
+    }()
+
+    private let contentView: UIView = {
+        let contentView = UIView()
+      //  screenView.backgroundColor = UIColor(named: "darkGreen")
+        contentView.toAutoLayout()
+        return contentView
+    }()
     
     private let workoutView: UIView = {
         let view = UIView()
@@ -34,7 +47,14 @@ class SelectedWorkoutViewController: UIViewController {
         return label
     }()
     
-    private let tableView = UITableView(frame: .zero, style: .plain)
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellID")
+        tableView.backgroundColor = UIColor(named: "lightGreen")
+        tableView.sizeToFit()
+        tableView.toAutoLayout()
+        return tableView
+    }()
     
     private let commentTextView: UITextView = {
        let textView = UITextView()
@@ -55,13 +75,17 @@ class SelectedWorkoutViewController: UIViewController {
         let button = UIButton()
         button.toAutoLayout()
         button.tintColor = .black
+//        button.backgroundColor = .white
         button.setImage(UIImage(systemName: "paperplane.fill"), for: .normal)
         button.addTarget(self, action: #selector(commentSent), for: .touchUpInside)
         return button
     }()
     
     @objc func commentSent() {
-        //to be done
+        guard let text = commentTextView.text else {return}
+        commentsArray.insert(text, at: 0)
+        commentTextView.text = ""
+        tableView.reloadData()
     }
 
     override func viewDidLoad() {
@@ -71,65 +95,93 @@ class SelectedWorkoutViewController: UIViewController {
         setupUI()
         setupSubviews()
         setupTableView()
+        setupToolbar()
     }
 
     
     private func setupUI() {
+
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add comment", style: .plain, target: self, action: #selector(addComment))
         view.backgroundColor = UIColor(named: "lightGreen")
+//        view.backgroundColor = .white
+ //       tabBarController?.tabBar.isHidden = true
     }
     
     private func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellID")
-        tableView.backgroundColor = UIColor(named: "lightGreen")
-        tableView.toAutoLayout()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        let h = self.commentTextView.contentSize.height
-        let limit = CGFloat(200)
-        
-        if h > limit {
-            self.commentTextView.isScrollEnabled = true
-        } else {
-            commentTextView.isScrollEnabled = false
-        }
     }
     
     private func setupSubviews() {
-        
+
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubviews(workoutView, tableView, commentTextView, addCommentButton)
         workoutView.addSubview(workoutDescriptionLabel)
-        view.addSubviews(workoutView, tableView, commentTextView, addCommentButton)
+//
+        let tableHeight = view.frame.height - workoutView.frame.height - commentTextView.frame.height - 450
         
         let constraints = [
             
-            workoutView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
-            workoutView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            workoutView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+ //           scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: 80),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            workoutView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            workoutView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            workoutView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
             
             workoutDescriptionLabel.topAnchor.constraint(equalTo: workoutView.topAnchor, constant: 10),
             workoutDescriptionLabel.leadingAnchor.constraint(equalTo: workoutView.leadingAnchor, constant: 10),
             workoutDescriptionLabel.trailingAnchor.constraint(equalTo: workoutView.trailingAnchor, constant: -10),
             workoutDescriptionLabel.bottomAnchor.constraint(equalTo: workoutView.bottomAnchor, constant: -10),
             
-            tableView.topAnchor.constraint(equalTo: workoutView.bottomAnchor, constant: 15),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-
-            commentTextView.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 15),
-            commentTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            commentTextView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -65),
-            commentTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -5),
+            commentTextView.topAnchor.constraint(equalTo: workoutView.bottomAnchor, constant: 15),
+            commentTextView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            commentTextView.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant: -70),
+            commentTextView.heightAnchor.constraint(lessThanOrEqualToConstant: 200),
             
-            addCommentButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
-            addCommentButton.widthAnchor.constraint(equalToConstant: 40),
-            addCommentButton.heightAnchor.constraint(equalToConstant: 40),
-            addCommentButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            addCommentButton.topAnchor.constraint(equalTo: workoutView.bottomAnchor, constant: 15),
+            addCommentButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            addCommentButton.widthAnchor.constraint(equalToConstant: 35),
+            addCommentButton.heightAnchor.constraint(equalToConstant: 35),
+            
+            tableView.topAnchor.constraint(equalTo: commentTextView.bottomAnchor, constant: 15),
+            tableView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            tableView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            tableView.heightAnchor.constraint(equalToConstant: tableHeight),
+            tableView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ]
         
         NSLayoutConstraint.activate(constraints)
+        
+        tableView.layoutIfNeeded()
+    }
+    
+    private func setupToolbar() {
+        let bar = UIToolbar()
+        
+        let doneBtn = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dissmissKeyboard))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        bar.items = [flexSpace, flexSpace, doneBtn]
+        bar.isTranslucent = true
+      //  bar.toAutoLayout()
+        bar.backgroundColor = .systemGray6
+        bar.tintColor = .black
+        bar.sizeToFit()
+        commentTextView.inputAccessoryView = bar
+    }
+    
+    @objc func dissmissKeyboard() {
+        view.endEditing(true)
     }
     
     @objc func addComment() {
@@ -139,7 +191,8 @@ class SelectedWorkoutViewController: UIViewController {
         navigationController?.pushViewController(newCommentVC, animated: true)
         newCommentVC.onSaveButtonPress = { [weak self] text in
             guard let self = self else {return}
-            self.commentsArray.append(text)
+            self.commentsArray.insert(text, at: 0)
+            print("\(self.commentsArray)")
             self.tableView.reloadData()
         }
     }
@@ -172,6 +225,42 @@ extension SelectedWorkoutViewController: UITableViewDelegate, UITableViewDataSou
     
     func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+    
+    // MARK: Keyboard
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+       // commentTextView.becomeFirstResponder()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc fileprivate func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            
+            scrollView.contentInset.bottom = keyboardSize.height + 40
+            scrollView.verticalScrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        }
+    }
+    
+    @objc fileprivate func keyboardWillHide(notification: NSNotification) {
+        scrollView.contentInset.bottom = .zero
+        scrollView.verticalScrollIndicatorInsets = .zero
+    }
+}
+
+extension SelectedWorkoutViewController: UITextViewDelegate {
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        return commentTextView.resignFirstResponder()
     }
 }
 
--- ECD_Training_Plan/Other/SceneDelegate.swift
+++ ECD_Training_Plan/Other/SceneDelegate.swift
@@ -15,13 +15,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        
+     
         // TODO: Update VC to sign in VC if not signed it
         let tabBarVC = TabBarController()
         window.rootViewController = tabBarVC
         window.makeKeyAndVisible()
         self.window = window
-        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
